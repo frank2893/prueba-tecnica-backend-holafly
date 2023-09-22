66c870c21fcabf9525f2a651dbcb49aaab04cb4e
Franklin <orlan2893@gmail.com>
2023-09-22 18:38:37 -0500

    feat: crear clase wookieePeople

---
+++ src/app/People/wookieePeople.js
@@ -0,0 +1,9 @@
+const AbstractPeople = require("./abstractPeople");
+
+class wookieePeople extends AbstractPeople {
+  constructor(id) {
+    throw new Error("To be implemented");
+  }
+}
+
+module.exports = wookieePeople
