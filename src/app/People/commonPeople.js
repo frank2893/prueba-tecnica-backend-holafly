0205e702516f877b9efa6f5a396154fa883636c2
Franklin <orlan2893@gmail.com>
2023-09-22 19:17:21 -0500

    feat: Agregar importacion de clase People, exportar CommonPeople, agregar variables

--- src/app/People/commonPeople.js
+++ src/app/People/commonPeople.js
@@ -1,5 +1,14 @@
+const AbstractPeople = require("./abstractPeople");
 class CommonPeople extends AbstractPeople {
-    constructor(id){
-        throw new Error("To be implemented");
-    }
+  constructor(id) {
+    super(id)
+    this.id = id;
+    this.name = null;
+    this.mass = null;
+    this.height = null;
+    this.homeworldName = null;
+    this.homeworlId = null;
+  }
 }
+
+module.exports = CommonPeople;
